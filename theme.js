8fcdbc99159edca0bcd3f3b3c2655c1c68eed49f
Aabhas Sao <aabhassao0@gmail.com>
2022-05-30 11:46:49 +0530

    chore: configure styled system and emotion
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

---
+++ theme.js
@@ -0,0 +1,19 @@
+const colors = {
+  primary: {
+    cyan: "#2acfcf",
+    darkViolet: "#3b3054"
+  },
+  secondary: {
+    red: "#f46262"
+  },
+  neutral: {
+    gray: "#bfbfbf",
+    grayishViolet: "#9e9aa7",
+    veryDarkBlue: "#35323e",
+    VeryDarkViolet: "#232127",
+  }
+}
+
+export default {
+  colors,
+}
